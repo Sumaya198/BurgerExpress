54091232700b5fa5987bcc7b922c5e1cff965ddb
Sumaya Hassan <sumaya_198@hotmail.co.uk>
2020-12-19 23:41:39 +0000

    adding JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,21 @@
+require('dotenv').config();
 const mysql = require('mysql');
+let connection;
 
-const password = process.argv[2];
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password,
-  database: "burger_db",
-});
-
-connection.connect(function (err) {
-    if (err) console.log("There is an error", err);
-    else console.log("connected!");
-  
-    connection.query("SELECT * FROM burgers", function (err, result) {
-      if (err) console.log("error", err);
-      else console.log("result");
-    });
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  const password = process.argv[2];
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password,
+    database: "burger_db",
   });
+}
+
+
+  connection.connect();
 
   module.exports = connection;
